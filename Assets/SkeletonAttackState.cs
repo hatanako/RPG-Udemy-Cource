596679655c9a81668096fb1daca4d058ce2ee85d
hatanako <2397663712@qq.com>
2025-06-16 17:10:52 +0800

    1

---
+++ Assets/SkeletonAttackState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAttackState : EnemyState
+{
+    private Enemy_Skeleton enemy;
+    public SkeletonAttackState(EnemyStateMachine _stateMachine, Enemy _enemyBase, string _animBoolName, Enemy_Skeleton _enemy) : base(_stateMachine, _enemyBase, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
